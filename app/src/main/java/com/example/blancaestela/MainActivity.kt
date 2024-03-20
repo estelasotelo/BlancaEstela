cadd29bac562f3331ac066cf5b5d4d59feb4305d
estelasotelo <blancasotelo.rfm@gmail.com>
2024-03-19 23:11:59 -0600

    Change the text of a Textview Use of Button Lifecycle

--- app/src/main/java/com/example/blancaestela/MainActivity.kt
+++ app/src/main/java/com/example/blancaestela/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.blancaestela
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("Lifecycle","OnCreate")
+        val greetingText = findViewById<TextView>(R.id.txt_output)
+        val actionButton = findViewById<Button>(R.id.btn_accion)
+        actionButton.setOnClickListener{
+            greetingText.text ="Welcome"
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Lifecycle","OnStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Lifecycle","OnResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Lifecycle","OnPause")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("Lifecycle","OnRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Lifecycle","OnDestroy")
     }
 }
